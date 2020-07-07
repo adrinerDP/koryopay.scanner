0335171e0b606992e070340e5f4560e171ff2d92
adrinerDP <me@adrinerdp.co>
2020-07-07 22:05:22 +0900

    Release 1.0.1

--- koryopay_reader.ino
+++ koryopay_reader.ino
@@ -5,11 +5,21 @@ MFRC522 rfid(10, 9);
 MFRC522::MIFARE_Key key;
 
 byte nuidPICC[4];
+int BUZZ = 3;
+int BLUE = 5;
+int RED  = 6;
 
 void setup() { 
   Serial.begin(9600);
   SPI.begin();
   rfid.PCD_Init();
+  
+  pinMode(BLUE, OUTPUT);
+  pinMode(RED, OUTPUT);
+  pinMode(BUZZ, OUTPUT);
+  
+  digitalWrite(BLUE, HIGH);
+  RunBuzzerDouble();
 }
  
 void loop() {
@@ -19,6 +29,8 @@ void loop() {
   if (!rfid.PICC_ReadCardSerial())
     return;
 
+  digitalWrite(BLUE, LOW);
+  digitalWrite(RED, HIGH);
   for (byte i = 0; i < 4; i++) {
     nuidPICC[i] = rfid.uid.uidByte[i];
   }
@@ -27,6 +39,11 @@ void loop() {
 
   rfid.PICC_HaltA();
   rfid.PCD_StopCrypto1();
+
+  RunBuzzerOnce();
+  delay(150);
+  digitalWrite(RED, LOW);
+  digitalWrite(BLUE, HIGH);
 }
 
 void printHex(byte *buffer, byte bufferSize) {
@@ -35,4 +52,20 @@ void printHex(byte *buffer, byte bufferSize) {
     Serial.print(buffer[i], HEX);
   }
   Serial.println();
+}
+
+void RunBuzzerOnce() {
+  digitalWrite(BUZZ, HIGH);
+  delay(50);
+  digitalWrite(BUZZ, LOW);
+}
+
+void RunBuzzerDouble() {
+  digitalWrite(BUZZ, HIGH);
+  delay(50);
+  digitalWrite(BUZZ, LOW);
+  delay(50);
+  digitalWrite(BUZZ, HIGH);
+  delay(50);
+  digitalWrite(BUZZ, LOW);
 }
